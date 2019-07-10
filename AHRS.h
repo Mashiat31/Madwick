9e1c79a9c83eb875700a5e43ec609abded3f6630
mash <mlamisa@connect.ust.hk>
2019-07-10 10:44:54 +0800

    added filter

--- AHRS.h
+++ AHRS.h
@@ -1,15 +1,28 @@
-/*
- * AHRS.h
- *
- *  Created on: Jul 10, 2019
- *      Author: Asus
- */
+#ifndef MADGWICK_AHRS
+#define MADGWICK_AHRS
 
-#ifndef AHRS_H_
-#define AHRS_H_
+typedef struct madgwickAhrs_t
+{
+    float beta;				// algorithm gain
+    float q0;
+    float q1;
+    float q2;
+    float q3;	            // quaternion of sensor frame relative to auxiliary frame
+    float invSampleFreq;
+    float roll;
+    float pitch;
+    float yaw;
+    char anglesComputed;
+} madgwickAhrs_t;
 
+void madgwickInit(madgwickAhrs_t *sensor, float beta, float freq);
+float getRoll(madgwickAhrs_t *sensor);
+float getPitch(madgwickAhrs_t *sensor);
+float getYaw(madgwickAhrs_t *sensor);
+float getRollRadians(madgwickAhrs_t *sensor);
+float getPitchRadians(madgwickAhrs_t *sensor);
+float getYawRadians(madgwickAhrs_t *sensor);
+void madgwickUpdateIMU(madgwickAhrs_t *sensor, float gx, float gy, float gz, float ax, float ay, float az);
+void madgwickUpdate(madgwickAhrs_t *sensor, float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
 
-
-
-
-#endif /* AHRS_H_ */
+#endif //MADGWICK_AHRS
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 /*
+
+
  * main.cpp
  *
  *  Created on: Jul 10, 2019
@@ -6,5 +8,26 @@
  */
 
 
+#include <iostream>
+#include <fstream>
+#include "csv.h"
+
+using namespace std;
+
+int main() {
+  io::CSVReader<7> in("test.csv");
+  in.read_header(io::ignore_missing_column, "Time stamp(ms)", "Ax", "Ay", "Az", "Gx", "Gy", "Gz");
+  int timestamp; double ax, ay, az, gx, gy, gz;
+
+  ofstream output_file;
+  output_file.open("result.csv");
+  output_file << "roll,pitch,yaw\n";
+  while(in.read_row(timestamp, ax, ay, az, gx, gy, gz)) {
+    output_file << ax << "," << gx << "," << gz << endl;
+  }
+  output_file.close();
+
+  return 0;
+}
 
 
